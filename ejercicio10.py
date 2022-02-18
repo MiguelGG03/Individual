d488de9c04d98502e7d714827c65736c295acdac
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-02-18 18:46:21 +0100

    ej 10 corregido

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,4 +1,7 @@
-base=float(input('Longitud de la base: '))
+import math
+lado=float(input('Longitud del lado: '))
 altura=float(input('Longitud de la altura: '))
+c=float(math.sqrt((lado)**2-(altura)**2))
+base=2*c
 area=(base*altura)/2
 print('El area de el triangulo es '+str(area))
